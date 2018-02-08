77c649bb21a2e686a8a77f894129bef642f1faa7
Sergey <nakul.sv@gmail.com>
2018-02-08 14:51:18 +0200

    made categories

---
+++ app/models/category.rb
@@ -0,0 +1,7 @@
+class Category < ApplicationRecord
+  VALID_NAME_REGEX = /\A[A-Z][a-z]+\s[a-z][a-z]+\.\z/i
+  validates :name, presence: true, format: { 
+  	with: VALID_NAME_REGEX,
+  	message: "at least 2 words in 2 letters, must include '.', the first word in uppercase."
+  	}
+end
